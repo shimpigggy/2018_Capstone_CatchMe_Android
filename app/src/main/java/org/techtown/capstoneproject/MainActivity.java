e6fd1b52e3f141b2bed72c5177793e8fbff83426
ShimPigggy <shimpig228@naver.com>
2018-05-20 05:24:24 +0900

    ShimSY: 문의하기(이메일, 빈칸), tabLayout(ViewPager -> FrameLayout)바꿈

--- app/src/main/java/org/techtown/capstoneproject/MainActivity.java
+++ app/src/main/java/org/techtown/capstoneproject/MainActivity.java
@@ -4,18 +4,26 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import org.techtown.capstoneproject.tab.Adapter;
+import org.techtown.capstoneproject.tab.Fragment_Email;
+import org.techtown.capstoneproject.tab.Fragment_Home;
+import org.techtown.capstoneproject.tab.Fragment_Search;
+import org.techtown.capstoneproject.tab.Fragment_Self;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private final int Home = 1;
+    private final int Search = 2;
+    private final int Self = 3;
+    private final int Email = 4;
+
 
     private TabLayout tabLayout;
-    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +35,13 @@ public class MainActivity extends AppCompatActivity {
         init();
         tabAdd();
 
-        // Creating TabPagerAdapter adapter
-        Adapter pagerAdapter = new Adapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        callFragment(Home);
 
         // Set TabSelectedListener
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+                callFragment(Integer.parseInt(tab.getTag() + ""));
             }
 
             @Override
@@ -49,23 +54,46 @@ public class MainActivity extends AppCompatActivity {
         });//addOnTabSelectedListener
     }//onCreate
 
-    public void permissionCheck(){
-        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+    public void permissionCheck() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
             //권한 없음
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA},0);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 0);
         }
     }
 
-    public void init(){
+    public void init() {
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        viewPager = (ViewPager) findViewById(R.id.pager);
     }
 
-    public void tabAdd(){
-        tabLayout.addTab(tabLayout.newTab().setText("Home"));
-        tabLayout.addTab(tabLayout.newTab().setText("Search"));
-        tabLayout.addTab(tabLayout.newTab().setText("Self"));
-        tabLayout.addTab(tabLayout.newTab().setText("EMail"));
+    public void tabAdd() {
+        tabLayout.addTab(tabLayout.newTab().setText("Home").setTag(Home));
+        tabLayout.addTab(tabLayout.newTab().setText("Search").setTag(Search));
+        tabLayout.addTab(tabLayout.newTab().setText("Self").setTag(Self));
+        tabLayout.addTab(tabLayout.newTab().setText("EMail").setTag(Email));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
+
+    private void callFragment(int num) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (num) {
+            case Home:
+                Fragment_Home fragment1 = new Fragment_Home();
+                transaction.replace(R.id.fragment_container, fragment1);
+                break;
+            case Search:
+                Fragment_Search fragment2 = new Fragment_Search();
+                transaction.replace(R.id.fragment_container, fragment2);
+                break;
+            case Self:
+                Fragment_Self fragment3 = new Fragment_Self();
+                transaction.replace(R.id.fragment_container, fragment3);
+                break;
+            case Email:
+                Fragment_Email fragment4 = new Fragment_Email();
+                transaction.replace(R.id.fragment_container, fragment4);
+                break;
+        }//switch
+        transaction.commit();
+    }//callFragment
 }//MainActivity
