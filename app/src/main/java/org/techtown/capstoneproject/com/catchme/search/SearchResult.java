f29b7b60b38fec37838dfbf72e0b3e4825c2653e
sj-kalin <hahava5696@gmail.com>
2018-05-23 08:10:11 +0900

    # ADD view search_result_view 각 성분에 대한 검색결과 view

--- app/src/main/java/org/techtown/capstoneproject/com/catchme/search/SearchResult.java
+++ app/src/main/java/org/techtown/capstoneproject/com/catchme/search/SearchResult.java
@@ -3,15 +3,22 @@ package org.techtown.capstoneproject.com.catchme.search;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.techtown.capstoneproject.R;
 
-public class SearchResult extends AppCompatActivity {
+public class SearchResult extends AppCompatActivity implements View.OnClickListener {
 
-    TextView tv;
     Intent intent;
+
+    RelativeLayout typeView;
+    RelativeLayout typeViewResult;
+    ImageView typeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +26,38 @@ public class SearchResult extends AppCompatActivity {
         setContentView(R.layout.activity_search_result);
         intent = getIntent();
         String word = intent.getStringExtra("gradiant");
-        tv = (TextView) findViewById(R.id.gradiant);
-        Toast.makeText(this,word,Toast.LENGTH_SHORT).show();
-        tv.setText(word);
+        Toast.makeText(this, word, Toast.LENGTH_SHORT).show();
+        typeView = (RelativeLayout) findViewById(R.id.type);
+        typeViewResult = (RelativeLayout) findViewById(R.id.type_result);
+        typeImage = (ImageView) findViewById(R.id.type_image);
+
+
+        typeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int visible = typeViewResult.getVisibility();
+                if (visible == View.VISIBLE) {
+                    typeViewResult.setVisibility(View.GONE);
+                    typeImage.setImageResource(R.drawable.arrow_down);
+                } else {
+                    typeViewResult.setVisibility(View.VISIBLE);
+                    typeImage.setImageResource(R.drawable.arrow_up);
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.alergy || v.getId() == R.id.functionfor || v.getId() == R.id.product) {
+            int visible = v.getVisibility();
+            if (visible == View.VISIBLE) {
+                v.setVisibility(View.GONE);
+            } else {
+                v.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
