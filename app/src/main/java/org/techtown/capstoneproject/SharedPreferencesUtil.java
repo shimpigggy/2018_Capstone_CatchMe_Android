ec211e0e80cf8718a93968bf26095b978946882d
shimpigggy <sd52461739>
2018-05-30 19:06:25 +0900

    SHIMSY

--- app/src/main/java/org/techtown/capstoneproject/SharedPreferencesUtil.java
+++ app/src/main/java/org/techtown/capstoneproject/SharedPreferencesUtil.java
@@ -24,6 +24,20 @@ public class SharedPreferencesUtil {
         editor.commit();
     }
 
+    //email 값을 가져오기
+    public static String getEmailPreferences(Context context, String name, String key) {
+        SharedPreferences p = context.getSharedPreferences(name, context.MODE_PRIVATE);
+        return p.getString(key, "해당 이메일 없음");
+    }
+
+    //email 값 저장
+    public static void saveEmailPreferences(Context context, String name, String key, String email) {
+        SharedPreferences p = context.getSharedPreferences(name, context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = p.edit();
+        editor.putString(key, email);
+        editor.commit();
+    }
+
     // 값(Key Data) 삭제하기
     public static void removePreferences(Context context, String name, String key) {
         SharedPreferences pref = context.getSharedPreferences(name, context.MODE_PRIVATE);
